c9d23e81d2aa6b97e1763d2b6d92c55b13495555
Timofey Kachalov <timofei@metabase.com>
2025-03-04 21:28:46 +0400

    Make e2e folder as subpackage (#19)

--- e2e/support/cypress.config.js
+++ e2e/support/cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress')
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: `http://localhost:${process.env.CLIENT_PORT}`,
-    supportFile: 'e2e/support/cypress.js',
-    specPattern: 'e2e/test/**/*.cy.spec.js',
+    supportFile: path.resolve(path.join(__dirname, './cypress.js')),
+    specPattern: path.resolve(path.join(__dirname, '../test/**/*.cy.spec.js')),
     defaultBrowser: 'chrome',
   },
 })
