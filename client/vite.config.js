b6b40e912ddf60d65409632aa1cf2368f33e8969
Timofey Kachalov <timofei@metabase.com>
2025-05-16 16:02:26 +0400

    Add e2e workflow + respect the WATCH env var when running in Docker (#28)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite";
+/* eslint-disable no-undef */
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    // eslint-disable-next-line no-undef
-    port: process.env.CLIENT_PORT
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  const devPort = parseInt(env.CLIENT_PORT, 10) || 3100
+  const previewPort = parseInt(env.CLIENT_PORT) || undefined
+
+  return {
+    plugins: [react()],
+    server: {
+      // eslint-disable-next-line no-undef
+      port: devPort
+    },
+    preview: {
+      port: previewPort
+    },
+  }
 });
